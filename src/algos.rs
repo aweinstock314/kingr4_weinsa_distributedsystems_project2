3cf5fdceac39592e4055a40079c1cd4280f9912c
23b0075nke <rking487@gmail.com>
2016-11-16 21:58:16 -0500

    Implemented client<-->server logic.

--- src/algos.rs
+++ src/algos.rs
@@ -1,38 +1,8 @@
 //FOR updating filesystem
 use std::sync::mpsc;
 use broadcasts::BroadcastAlgorithm;
-use std::collections::{HashSet, HashMap};
-use std::hash::Hash; 
+use std::collections::{HashMap};
 
-
-// Algos.rs 
-// Catchall for algo implementation, logic to oversee network & 
-// execute ZAB
-// "pure code that does all the scheduling stuff"
-// Make use of broadcast to get pid/message pairs to broadcast, broad
-
-// HandleMessage trait for protocols that can handle messages
-// Compartmentalize: logic for how and when to send and receive data
-// Manage queues of messages and filesystem updates to maintain FIFO, two phase broadcast/commit 
-// Need explicit indices on messages 
-
-
-// 2-phase commit + FIFO RB
-// BROADCAST of VALUE (v): 
-/*
-    On broadcast: generate transaction (value, (epoch, counter)) 
-        Send proposal to all followers.
-
-    On recv msg (val, (ep, c)):
-        Queue val
-        Send acknowledgement to leader
-        Wait for commit
-
-    On recv ack: 
-        (Safety - if leader:) 
-            If majority, sendall commit + msg (value, (epoch, counter))
-
-*/
 pub trait HandleMessage {
     type Pid;
     type Message;
@@ -48,18 +18,18 @@ pub trait HandleMessage {
 // Connections list for broadcast? 
 // Filesystem feeds pid/file data to algo for handling 
 // Used by control thread (main)
-pub struct System<B:BroadcastAlgorithm<UnderlyingMessage = PeerToPeerMessage>>{
+pub struct System<B:BroadcastAlgorithm<UnderlyingMessage = PeerToPeerMessage>> {
 	broadcast: B, 
     files: HashMap<String, String>, // Files that exist in the system - <name, content> 
     receiver: mpsc::Receiver<PeerToPeerMessage>,
 }
 
 
-impl <B:Default+BroadcastAlgorithm<UnderlyingMessage = PeerToPeerMessage>> System<B>{ //"Default" for constructors w.o args
-	 fn new() -> System<B>{ //constructor so we define the delivery (trigger update)
+impl<B:BroadcastAlgorithm<UnderlyingMessage = PeerToPeerMessage>> System<B> { //"Default" for constructors w.o args
+	 pub fn new(broadcast: B) -> System<B>{ //constructor so we define the delivery (trigger update)
     	let(transmit, receive) = mpsc::channel(); // channel as a "local socket". used instead of mutex, similar concept
-    	let mut s = System{
-    		broadcast:B::default(),
+    	let mut s = System {
+    		broadcast: broadcast,
 			files: HashMap::new(),
 			receiver: receive,
 		};
@@ -69,23 +39,51 @@ impl <B:Default+BroadcastAlgorithm<UnderlyingMessage = PeerToPeerMessage>> Syste
 		s
     }
 
-    //create/delete/append filename, return confirmatoin in form of "%filename %task" + "ed"
-	/*fn update(task, filename) -> message(clientpid, confirmation string)) {
-
-	}
-
 	// Takes in messages from clients 
-	//		create fname | delete fname | read fname | append fname value | exit (possibly? might be able to gracefully handle dropped sockets
+	//		create fname | delete fname | read fname | append fname value 
 	// 		Invokes ZAB
 	//			Forwards client message to the leader as a p2p message with the related data.
-    fn handleClientMessage( /* takes client2server msg */ ) -> /* (peer2peer pids & msgs, server2client msgs) */ {
-	
-    }*/
+    pub fn handle_client_message(&mut self, m: ClientToServerMessage) -> (Vec<(<Self as HandleMessage>::Pid, <Self as HandleMessage>::Message)>, Vec<ServerToClientMessage>) {
+    	let mut peer_messages = vec![];
+    	let mut client_messages = vec![];
+
+		match m {
+			ClientToServerMessage::Create(filename) => {
+				peer_messages.extend(self.broadcast.broadcast(&PeerToPeerMessage::Create(filename)));
+			},
+			ClientToServerMessage::Delete(filename) => {
+				peer_messages.extend(self.broadcast.broadcast(&PeerToPeerMessage::Delete(filename)));
+			},
+			ClientToServerMessage::Append(filename, data) => {
+				peer_messages.extend(self.broadcast.broadcast(&PeerToPeerMessage::Append(filename, data)));
+			},
+			ClientToServerMessage::Read(filename) => {
+				if let Some(content) = self.files.get(&filename){
+					client_messages.push(ServerToClientMessage::HumanDisplay(format!("Received data from {}: {}",filename, content)));
+				} else {
+					client_messages.push(ServerToClientMessage::HumanDisplay(format!("{} does not exist.", filename)));
+				}
+			}
+		}
+		(peer_messages, client_messages)
+   	}
+}
+
+
+pub enum ServerToClientMessage {
+	HumanDisplay(String),
+}
+
+pub enum ClientToServerMessage {
+	Create(String),
+	Delete(String),
+	Append(String, String),
+	Read(String),
 }
 
 // (create | delete | append)
 #[derive(Clone)]
-pub enum PeerToPeerMessage{
+pub enum PeerToPeerMessage {
 	Create(String),
 	Delete(String),
 	Append(String, String),
@@ -93,18 +91,18 @@ pub enum PeerToPeerMessage{
 
 // Message bookkeeping interface - receives message data, pulls new message data for the handler to send
 // Can distinguish between message types at network level
-impl<B:BroadcastAlgorithm<UnderlyingMessage = PeerToPeerMessage>> HandleMessage for System<B>{
+impl<B: BroadcastAlgorithm<UnderlyingMessage=PeerToPeerMessage>> HandleMessage for System<B> {
 	type Pid = B::Pid;
 	type Message = B::Message;
     fn handle_message(&mut self, m: &Self::Message) -> Vec<(Self::Pid, Self::Message)> {
     	//if create, put a file in system, etc
-    	let toSend = self.broadcast.handle_message(m);
+    	let to_send = self.broadcast.handle_message(m);
     	while let Ok(msg) = self.receiver.try_recv(){
     		match msg{
     			PeerToPeerMessage::Create(filename) => {
     				self.files.entry(filename).or_insert("".into());
     			},
-    			PeerToPeerMessage::Delete(filename) =>{
+    			PeerToPeerMessage::Delete(filename) => {
     				self.files.remove(&filename);
     				// TODO - error handling (file does not exist)
 
@@ -112,15 +110,14 @@ impl<B:BroadcastAlgorithm<UnderlyingMessage = PeerToPeerMessage>> HandleMessage 
     			PeerToPeerMessage::Append(filename, data) => {
     				if let Some(content) = self.files.get_mut(&filename){
     					content.push_str(data.as_str());
-    				}
-    				else{
+    				} else{
     					// TODO - error handling
     				}
 
     			},
     		}
     	}
-    	
-    	toSend
+
+    	to_send
     }
 }
