6b14ad82fa8859c7f32277fd8cdcc2a65a846437
23b0075nke <rking487@gmail.com>
2016-11-16 21:16:21 -0500

    Implemented peer-to-peer message handling, added notes for functionality, added ZAB pseudocode.

--- src/algos.rs
+++ src/algos.rs
@@ -1,22 +1,126 @@
+//FOR updating filesystem
+use std::sync::mpsc;
+use broadcasts::BroadcastAlgorithm;
+use std::collections::{HashSet, HashMap};
+use std::hash::Hash; 
+
+
+// Algos.rs 
+// Catchall for algo implementation, logic to oversee network & 
+// execute ZAB
+// "pure code that does all the scheduling stuff"
+// Make use of broadcast to get pid/message pairs to broadcast, broad
+
 // HandleMessage trait for protocols that can handle messages
+// Compartmentalize: logic for how and when to send and receive data
+// Manage queues of messages and filesystem updates to maintain FIFO, two phase broadcast/commit 
+// Need explicit indices on messages 
+
+
+// 2-phase commit + FIFO RB
+// BROADCAST of VALUE (v): 
+/*
+    On broadcast: generate transaction (value, (epoch, counter)) 
+        Send proposal to all followers.
+
+    On recv msg (val, (ep, c)):
+        Queue val
+        Send acknowledgement to leader
+        Wait for commit
+
+    On recv ack: 
+        (Safety - if leader:) 
+            If majority, sendall commit + msg (value, (epoch, counter))
+
+*/
 pub trait HandleMessage {
     type Pid;
     type Message;
-    fn handle_message(&mut self, m: &Self::Message) -> Vec<(Self::Pid, Self::Message)>;
+    fn handle_message(&mut self, m: &Self::Message) -> Vec<(Self::Pid, Self::Message)>; //destination + message for output of handlemessage 
 }
 
-/*
-struct FileSystem {
-    files: HashMap<String, String>
-    // also have bcast impl, peers, etc
-}
-
+// To draw from for message sending, etc.
+// Implemented as a data structure for ease of testing, portability on EC2 instances 
+//template over whatever broadcast it's using, then use that to invoke broadcast
+// has map of peer IDs for reference 
 // TODO: client to server enum
+// An enum in Rust is a type that represents data that is one of several possible variants. Each variant in the enum can optionally have data associated with it
+// Connections list for broadcast? 
+// Filesystem feeds pid/file data to algo for handling 
+// Used by control thread (main)
+pub struct System<B:BroadcastAlgorithm<UnderlyingMessage = PeerToPeerMessage>>{
+	broadcast: B, 
+    files: HashMap<String, String>, // Files that exist in the system - <name, content> 
+    receiver: mpsc::Receiver<PeerToPeerMessage>,
+}
 
-impl FileSystem {
-    fn process( /* takes client2server msg */ ) -> /* (peer2peer pids & msgs, server2client msgs) */ {
+
+impl <B:Default+BroadcastAlgorithm<UnderlyingMessage = PeerToPeerMessage>> System<B>{ //"Default" for constructors w.o args
+	 fn new() -> System<B>{ //constructor so we define the delivery (trigger update)
+    	let(transmit, receive) = mpsc::channel(); // channel as a "local socket". used instead of mutex, similar concept
+    	let mut s = System{
+    		broadcast:B::default(),
+			files: HashMap::new(),
+			receiver: receive,
+		};
+		s.broadcast.set_on_deliver(Box::new(move |msg|{
+			transmit.send(msg.clone()).unwrap();
+			}));
+		s
+    }
+
+    //create/delete/append filename, return confirmatoin in form of "%filename %task" + "ed"
+	/*fn update(task, filename) -> message(clientpid, confirmation string)) {
+
+	}
+
+	// Takes in messages from clients 
+	//		create fname | delete fname | read fname | append fname value | exit (possibly? might be able to gracefully handle dropped sockets
+	// 		Invokes ZAB
+	//			Forwards client message to the leader as a p2p message with the related data.
+    fn handleClientMessage( /* takes client2server msg */ ) -> /* (peer2peer pids & msgs, server2client msgs) */ {
+	
+    }*/
+}
+
+// (create | delete | append)
+#[derive(Clone)]
+pub enum PeerToPeerMessage{
+	Create(String),
+	Delete(String),
+	Append(String, String),
+}
+
+// Message bookkeeping interface - receives message data, pulls new message data for the handler to send
+// Can distinguish between message types at network level
+impl<B:BroadcastAlgorithm<UnderlyingMessage = PeerToPeerMessage>> HandleMessage for System<B>{
+	type Pid = B::Pid;
+	type Message = B::Message;
+    fn handle_message(&mut self, m: &Self::Message) -> Vec<(Self::Pid, Self::Message)> {
+    	//if create, put a file in system, etc
+    	let toSend = self.broadcast.handle_message(m);
+    	while let Ok(msg) = self.receiver.try_recv(){
+    		match msg{
+    			PeerToPeerMessage::Create(filename) => {
+    				self.files.entry(filename).or_insert("".into());
+    			},
+    			PeerToPeerMessage::Delete(filename) =>{
+    				self.files.remove(&filename);
+    				// TODO - error handling (file does not exist)
+
+    			},
+    			PeerToPeerMessage::Append(filename, data) => {
+    				if let Some(content) = self.files.get_mut(&filename){
+    					content.push_str(data.as_str());
+    				}
+    				else{
+    					// TODO - error handling
+    				}
+
+    			},
+    		}
+    	}
+    	
+    	toSend
     }
 }
-*/
-
-// also have impl of HandleMessage for peer2peer things
--- src/broadcasts.rs
+++ src/broadcasts.rs
@@ -1,8 +1,20 @@
+// Broadcasts.rs 
+// Broadcast functionality - interfaces with networking level.
+// Contains pointwise sendall, methods for sending different types of messages 
+
+//for algorithimic message handling (ZAB, etc) 
+
 use algos::HandleMessage;
 use std::collections::HashSet;
 use std::hash::Hash;
 use std::marker::PhantomData;
 
+
+// Trait = interface 
+// Define: message set, sequence track 
+//   Deliver when: recv commit, counter is "in the right order"
+// Anything that implements broadcast also needs to handlemessage
+// Subtrait of handlemessage, implementations of Broadcast need to also implement HM
 pub trait BroadcastAlgorithm: HandleMessage {
     type UnderlyingMessage;
     fn set_on_deliver(&mut self, Box<FnMut(&Self::UnderlyingMessage)>);
@@ -10,7 +22,6 @@ pub trait BroadcastAlgorithm: HandleMessage {
 }
 
 // pointwise sendall implementation
-
 pub struct SendAll<Pid, Msg> {
     deliver: Option<Box<FnMut(&Msg)>>,
     ownpid: Pid,
@@ -18,6 +29,7 @@ pub struct SendAll<Pid, Msg> {
     _msgtype: PhantomData<Msg>,
 }
 
+// Impl define struct methods
 impl<Pid, Msg> SendAll<Pid, Msg> {
     pub fn new(ownpid: Pid, neighbors: HashSet<Pid>) -> SendAll<Pid, Msg> {
         SendAll {
@@ -35,6 +47,9 @@ pub struct SendAllMessage<Pid, Msg> {
     underlying: Msg,
 }
 
+// For sendall
+// Takes broadcast and message as arguments, returns a vector of pid/message tuples 
+// implementation of handlemessage for broadcast algo
 impl<Pid, Msg> HandleMessage for SendAll<Pid, Msg> {
     type Pid = Pid;
     type Message = SendAllMessage<Pid, Msg>;
@@ -63,4 +78,37 @@ impl<Pid: Eq+Hash+Copy, Msg: Clone> BroadcastAlgorithm for SendAll<Pid, Msg> {
     }
 }
 
-// TODO: ZAB bcast goes below here
+
+// struct for ZAB/FIFO that stores relevant state stuff (counters, etc.) 
+//      process counter to send on messages
+//      ack counter
+//      next queue (of process counters)
+//      process queue (to deliver FIFO)
+   // processes: HashMap<String, String>, // Processes to communicate with
+    //leader: usize // leader PID 
+
+//need default impl for broadcast (zab)
+
+// whatever zab broadcast handling logic function: 
+    // Takes peer to peer messages and returns confirmation for server2client 
+    //   peer -> peer msgs (if sessions are non-atomic): broadcast of individual (create | delete | append)
+        // If leader:
+        //      If message is ack, and ack counter is >= total processes/2:
+        //          send commit to all in processes
+        //      If message is ack, and ack counter is <= total processes/2:
+        //          ackcounter++
+        //      If message is direct send from a peer:
+        //          broadcast:send( system:getSendAll(content)) 
+        // Else:
+        //      If message is sendall from leader:
+        //          store in message queue 
+        //          return ack
+        //      If message is commit from leader: (assume that message has already been delivered?)
+        //          loop through message queue to see if messagecounter is next
+        //          if next, deliver message (filesystem::update)
+//client message handling for zab: 
+//      // leader = FileSystem:GetLeader()
+        //      If leader is not self:
+        //          fwd message to leader using broadcast:sendmessage
+        //      Else:
+        //          loop through result of system:getall and broadcast:send to each (or broadcast:sendall message, since that already deals with it?)
