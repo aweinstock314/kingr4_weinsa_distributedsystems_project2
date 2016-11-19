43466f565141aae1d46a4a0f63a57fa46f6195b7
23b0075nke <rking487@gmail.com>
2016-11-19 17:42:36 -0500

    Added virtual and write-to-disk message logs to System. Clarified some comments.

--- src/algos.rs
+++ src/algos.rs
@@ -1,7 +1,15 @@
-//FOR updating filesystem
+// Algos.rs (TODO - rename)
+// Functionality for maintaining, updating filesystem. 
+// Also helps determine what message content should be broadcast to whom.
+
+use std::fs::File;
+use std::fs::OpenOptions;
+use std::io::Write;
 use std::sync::mpsc;
 use broadcasts::BroadcastAlgorithm;
 use std::collections::{HashMap};
+use serde_json;
+
 
 pub trait HandleMessage {
     type Pid;
@@ -9,29 +17,32 @@ pub trait HandleMessage {
     fn handle_message(&mut self, m: &Self::Message) -> Vec<(Self::Pid, Self::Message)>; //destination + message for output of handlemessage 
 }
 
-// To draw from for message sending, etc.
-// Implemented as a data structure for ease of testing, portability on EC2 instances 
-//template over whatever broadcast it's using, then use that to invoke broadcast
-// has map of peer IDs for reference 
-// TODO: client to server enum
-// An enum in Rust is a type that represents data that is one of several possible variants. Each variant in the enum can optionally have data associated with it
-// Connections list for broadcast? 
-// Filesystem feeds pid/file data to algo for handling 
-// Used by control thread (main)
+// System:
+// Used by control thread (main). 
+// Keeps track of - logical filesystem, process message logs. 
+// Also carries broadcast to invoke, reciever channel use.
+// Templated over (a) broadcast implementation in broadcasts.rs. 
 pub struct System<B:BroadcastAlgorithm<UnderlyingMessage = PeerToPeerMessage>> {
-	broadcast: B, 
-    files: HashMap<String, String>, // Files that exist in the system - <name, content> 
-    receiver: mpsc::Receiver<PeerToPeerMessage>,
+	broadcast: B, // broadcast algorithm, to access/send messages
+    files: HashMap<String, String>, // Maps filename -> filecontent
+    receiver: mpsc::Receiver<PeerToPeerMessage>, 
+    log: Vec<PeerToPeerMessage>, // Virtual log of messages sent/recieved since last save-to-disk. Maps 
+    disk_log: File,// File loaded on startup, written version of virtual log
 }
 
+// Implementation of System constructor, message handler (which updates the filesystem conditionally)
+impl<B:BroadcastAlgorithm<UnderlyingMessage = PeerToPeerMessage>> System<B> { 
+	 pub fn new(broadcast: B) -> System<B>{ 
+    	let(transmit, receive) = mpsc::channel();
+    	let logname = format!("log{}.txt", 0); // TODO - PID
+    	let file = OpenOptions::new().append(true).read(true).create(true).open(logname).expect("Error in System constructor, could not open log file on disk.");
 
-impl<B:BroadcastAlgorithm<UnderlyingMessage = PeerToPeerMessage>> System<B> { //"Default" for constructors w.o args
-	 pub fn new(broadcast: B) -> System<B>{ //constructor so we define the delivery (trigger update)
-    	let(transmit, receive) = mpsc::channel(); // channel as a "local socket". used instead of mutex, similar concept
     	let mut s = System {
     		broadcast: broadcast,
 			files: HashMap::new(),
 			receiver: receive,
+			log: Vec::new(),
+			disk_log: file,
 		};
 		s.broadcast.set_on_deliver(Box::new(move |msg|{
 			transmit.send(msg.clone()).unwrap();
@@ -39,10 +50,9 @@ impl<B:BroadcastAlgorithm<UnderlyingMessage = PeerToPeerMessage>> System<B> { //
 		s
     }
 
-	// Takes in messages from clients 
-	//		create fname | delete fname | read fname | append fname value 
-	// 		Invokes ZAB
-	//			Forwards client message to the leader as a p2p message with the related data.
+	// Takes in messages from clients: create fname | delete fname | read fname | append fname value 
+	// Invokes ZAB
+	// Forwards client message to the leader as a p2p message with the related data.
     pub fn handle_client_message(&mut self, m: ClientToServerMessage) -> (Vec<(<Self as HandleMessage>::Pid, <Self as HandleMessage>::Message)>, Vec<ServerToClientMessage>) {
     	let mut peer_messages = vec![];
     	let mut client_messages = vec![];
@@ -69,6 +79,8 @@ impl<B:BroadcastAlgorithm<UnderlyingMessage = PeerToPeerMessage>> System<B> { //
    	}
 }
 
+// Message enums: 
+// Used to distinguish message types between different parties 
 
 pub enum ServerToClientMessage {
 	HumanDisplay(String),
@@ -82,35 +94,44 @@ pub enum ClientToServerMessage {
 }
 
 // (create | delete | append)
-#[derive(Clone)]
+#[derive(Clone, Serialize, Deserialize)]
 pub enum PeerToPeerMessage {
 	Create(String),
 	Delete(String),
 	Append(String, String),
 }
 
-// Message bookkeeping interface - receives message data, pulls new message data for the handler to send
-// Can distinguish between message types at network level
+// HandleMessage: 
+// Message bookkeeping interface - receives message data, pulls new message data for the handler to send/
+// Returns to_send***
 impl<B: BroadcastAlgorithm<UnderlyingMessage=PeerToPeerMessage>> HandleMessage for System<B> {
 	type Pid = B::Pid;
 	type Message = B::Message;
     fn handle_message(&mut self, m: &Self::Message) -> Vec<(Self::Pid, Self::Message)> {
-    	//if create, put a file in system, etc
+    	// Deliver message (edit files) as necessary. 
     	let to_send = self.broadcast.handle_message(m);
     	while let Ok(msg) = self.receiver.try_recv(){
+    		// Add message to virtual log. 
+    		self.log.push(msg.clone());
+
+    		// Add message to disk log. (AT THE MOMENT - do so after every message, rather than flushing the log periodically.)
+    		// Possible TODO - update ofr efficiency. 
+    		write!(self.disk_log, "{}\n", serde_json::to_string(&msg).unwrap()).expect("Error in System::HandleMessage. Could not write to disk.");
+
+    		// Edit virtual log as specified: 
     		match msg{
     			PeerToPeerMessage::Create(filename) => {
-    				self.files.entry(filename).or_insert("".into());
+    				self.files.entry(filename).or_insert("".into());		
+					// Write log to disk
     			},
     			PeerToPeerMessage::Delete(filename) => {
     				self.files.remove(&filename);
     				// TODO - error handling (file does not exist)
-
     			},
     			PeerToPeerMessage::Append(filename, data) => {
     				if let Some(content) = self.files.get_mut(&filename){
     					content.push_str(data.as_str());
-    				} else{
+    				} else {
     					// TODO - error handling
     				}
 
