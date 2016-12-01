760788f5192f44cc3729b31b6a1a910496dfffc1
23b0075nke <rking487@gmail.com>
2016-12-01 09:30:55 -0500

    debug version

--- setup_aws.sh
+++ setup_aws.sh
@@ -3,22 +3,22 @@
 IPS_ZONE1="54.183.96.145 54.67.110.56"
 IPS_ZONE2="52.15.170.53 52.15.124.72"
 
-cargo build --release
+cargo build
 
 for ip in $IPS_ZONE1; do
-    KEY=demo1.pem
-    echo scp -i "$KEY" target/release/kingr4_weinsa_distributedsystems_project2 "ubuntu@${ip}:project2"
+    KEY=demo3.pem
+    echo scp -i "$KEY" target/debug/kingr4_weinsa_distributedsystems_project2 "ubuntu@${ip}:project2"
     echo scp -i "$KEY" demo_nodes.txt "ubuntu@${ip}:nodes.txt"
 done
 
 for ip in $IPS_ZONE2; do
-    KEY=demo3.pem
-    echo scp -i "$KEY" target/release/kingr4_weinsa_distributedsystems_project2 "ubuntu@${ip}:project2"
+    KEY=demo1.pem
+    echo scp -i "$KEY" target/debug/kingr4_weinsa_distributedsystems_project2 "ubuntu@${ip}:project2"
     echo scp -i "$KEY" demo_nodes.txt "ubuntu@${ip}:nodes.txt"
 done
 
 
-echo ssh -i demo1.pem ubuntu@54.183.96.145 '"export RUST_LOG=\"kingr4_weinsa_distributedsystems_project2=debug\"; screen -S project2 -d -m; sleep 0.1; screen -S project2 -p 0 -X stuff \"./target/debug/kingr4_weinsa_distributedsystems_project2 server 1\\n\""'
-echo ssh -i demo1.pem ubuntu@54.67.110.56 '"export RUST_LOG=\"kingr4_weinsa_distributedsystems_project2=debug\"; screen -S project2 -d -m; sleep 0.1; screen -S project2 -p 0 -X stuff \"./target/debug/kingr4_weinsa_distributedsystems_project2 server 2\\n\""'
-echo ssh -i demo3.pem ubuntu@52.15.170.53 '"export RUST_LOG=\"kingr4_weinsa_distributedsystems_project2=debug\"; screen -S project2 -d -m; sleep 0.1; screen -S project2 -p 0 -X stuff \"./target/debug/kingr4_weinsa_distributedsystems_project2 server 3\\n\""'
-echo ssh -i demo3.pem ubuntu@52.15.124.72 '"export RUST_LOG=\"kingr4_weinsa_distributedsystems_project2=debug\"; screen -S project2 -d -m; sleep 0.1; screen -S project2 -p 0 -X stuff \"./target/debug/kingr4_weinsa_distributedsystems_project2 server 4\\n\""'
+echo ssh -i demo3.pem ubuntu@ec2-54-183-96-145.us-west-1.compute.amazonaws.com '"export RUST_LOG=\"kingr4_weinsa_distributedsystems_project2=debug\"; screen -S project2 -d -m; sleep 0.1; screen -S project2 -p 0 -X stuff \"./project2 server 1\\n\""'
+echo ssh -i demo3.pem ubuntu@ec2-54-67-110-56.us-west-1.compute.amazonaws.com '"export RUST_LOG=\"kingr4_weinsa_distributedsystems_project2=debug\"; screen -S project2 -d -m; sleep 0.1; screen -S project2 -p 0 -X stuff \"./project2 server 2\\n\""'
+echo ssh -i demo1.pem ubuntu@ec2-52-15-170-53.us-east-2.compute.amazonaws.com '"export RUST_LOG=\"kingr4_weinsa_distributedsystems_project2=debug\"; screen -S project2 -d -m; sleep 0.1; screen -S project2 -p 0 -X stuff \"./project2 server 3\\n\""'
+echo ssh -i demo1.pem ubuntu@ec2-52-15-124-72.us-east-2.compute.amazonaws.com '"export RUST_LOG=\"kingr4_weinsa_distributedsystems_project2=debug\"; screen -S project2 -d -m; sleep 0.1; screen -S project2 -p 0 -X stuff \"./project2 server 4\\n\""'
